077677d7347b2cdf1c0cd9c5efc4cb5fa6736f47
Min-Hsuan <88972447+Min-Hsuan@users.noreply.github.com>
2025-04-22 22:58:03 +0800

    add loading bar

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+export default function Loading(){
+    return(
+        <div className="fbg-gray-800 text-white w-full h-full z-50 transition-[0.5] flex justify-center flex-col">
+            <h3 className="text-3xl font-bold tracking-wider relative ">Tsai Min-Hsuan</h3>
+            <span className="block w-1/0 h-2 bg-orange-500 absolute bottom-[-30px] left-0"></span>
+            <span className="text-sm font-normal absolute bottom-[-32px] left-1/2 ml-2">Portfolio</span>
+
+        </div>
+    )
+}
