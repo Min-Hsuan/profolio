93ef5a501ade2306b039d6eacc963d5fc017e0b4
Min-Hsuan <88972447+Min-Hsuan@users.noreply.github.com>
2025-04-22 22:58:48 +0800

    change style of switch button

---
+++ src/components/SwitchButton.jsx
@@ -0,0 +1,28 @@
+export default function SwitchButton({modeId, switchMode}){
+    const modes = [
+        'Web Design', 'Graphic Design'
+      ]
+    return (
+        <nav className="flex items-center justify-center w-48 h-12 bg-gray-600 text-gray-900 rounded-full mx-auto mt-4" role="navigation" aria-label="Switch between Web and Graphic Design">
+            <button className={`relative flex items-center justify-center w-1/2 h-full rounded-full transition-all duration-300 ${modeId === 0 ? 'bg-amber-600 text-black shadow-md': ''}`}
+            onClick={()=>switchMode(-modeId)}
+            tabIndex={0}
+            onKeyDown={(e)=>e.key === "Enter" && switchMode(-modeId)}
+            aria-label="Switch to Web Design"
+            aria-current={modeId === 0 ? 'true': 'false'}
+            >
+                <span>Web Design</span>
+            </button>
+            <button className={`relative flex items-center justify-center w-1/2 h-full rounded-full transition-all duration-300 ${modeId === 1 ? 'bg-amber-600 text-black shadow-md': ''}`}
+            onClick={()=>switchMode(1-modeId)}
+            tabIndex={0}
+            onKeyDown={(e)=>e.key === "Enter" && switchMode(1-modeId)}
+            aria-label="Switch to Graphic Design"
+            aria-current={modeId === 1 ? 'true': 'false'}
+            >
+                <span>Graphic Design</span>
+            </button>
+
+        </nav>
+    )
+}
